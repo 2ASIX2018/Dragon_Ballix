cd85160d5118180a85cca0a0bcd0b9aa3a4bf935
Kenny-asix <jorgesengom@gmail.com>
2018-11-08 19:50:51 +0100

    Trabajando en Sprint2

---
+++ Dragon_Ballix/registro.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta charset="utf-8">
+
+    <title>Registrate</title>
+
+    <link rel="stylesheet" href="css/login.css" type="text/css" media="all">
+
+    <link rel="shortcut icon" href="img/favicon.ico" />
+
+</head>
+
+<body>
+
+<form action="mailto:emaildelaempresaquehaceelformulario@email.com" method="post" enctype="text/plain">
+
+<p class="regis">Nombre</p><input type="text" name="nombre" size="50" maxlength="50"><br><br>
+<p class="regis">Apellidos</p> <input type="text" name="apellidos" size="50" maxlength="50">
+<br><br>
+<p class="regis">Correo electrónico</p> <input type="text" value="" name="correo" size="50" maxlength="100">
+<br><br>
+<p class="regis">Nombre de usuario</p> <input type="text" name="user" size="15" maxlength="50">
+<br><br>
+<p class="regis">Contraseña</p> <input type="password" name="passwd" size="15" maxlength="50">
+<br><br>
+<p class="regis">Confirmar contraseña</p> <input type="password" name="confirm" size="15" maxlength="50">
+<br>
+<br>
+<table width="50%" border="0" align="center" cellpadding="10" cellspacing="0">
+<tr>
+<div align="left">
+<input type="submit" value="Enviar formulario">
+</div>
+<br><br>
+<div align="left">
+<input type="Reset" value="Borrar formulario">
+</div>
+</tr>
+</table>
+</form>
+</body>
+</html>
+
+
+
