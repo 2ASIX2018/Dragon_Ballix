66fe9549866f516bb1fe7fc91abcb1aa5586aec6
Kenny-asix <43198633+Kenny-asix@users.noreply.github.com>
2019-02-27 22:47:00 +0100

    Add files via upload

--- Dragon_Ballix/index.php
+++ Dragon_Ballix/index.php
@@ -1,106 +1,20 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta content="Jorge Sendra Gómez" name="author">
-		<meta content="DragonBallWorld" name="description">
-		<meta content="Dragon ball" name="keywords">
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" href="css/general.css" type="text/css" media="all">
-		
-    <link rel="shortcut icon" href="img/favicon.ico" />
-		
-		<title>DragonBallWorld</title>
-    </head>
+<?php get_header(); ?>
+<main class="wrap">
+  <section class="content-area content-thin col-md-9" style="float:left; margin-top:50px;">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <article class="article-loop">
+        <header>
+          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          Autor: <?php the_author(); ?>
+        </header>
+        <?php the_excerpt(); ?>
+      </article>
+<?php endwhile; else : ?>
+      <article>
+        <p>Lo siento pero encontrarás nada</p>
+      </article>
+<?php endif; ?>
+  </section><?php get_sidebar(); ?>
+</main>
+<?php get_footer(); ?>
 
-    <body>
-            <p class="stroke1">Drag</p><p class="stroke2">o</p><p class="stroke1">n</p><p class="stroke3">BallW</p><p class="stroke2">o</p><p class="stroke3">rld</p>
-            
-            <br>
-            <br>
-          
-            <nav class="main-nav">
-                
-            <?php
-
-            require_once("menu.php");
-
-            ?>
-        </nav>
-            <br>
-                 <br>
-                 
-            
-            
-            <p class="stroke4">Intr<p class="stroke5">o</p><p class="stroke4">ducci</p><p class="stroke5">o</p><p class="stroke4">n</p>
-            
-            <p>Se bienvenido a <strong>Dragon Ball World</strong>, una página donde encontrarás información detallada sobre aquello que busques sobre el universo de<strong> Dragon ball</strong>, además de nuestro exclusivo foro en el que podrás debatir con más personas sobre diversos temas.</p>
-        
-<br>
-        
-        <p class="stroke1">Akira</p><p class="stroke3">T</p><p class="stroke2">o</p><p class="stroke3">riyama</p>
-        
-        <br>
-        
-        <img src="img/Akira.jpg" height="250px" weight="250px"/>
-        
-        
-        <p>Akira Toriyama es un dibujante de manga y diseñador artístico. Es conocido mundialmente, en el mundo del anime, por sus obras Dr. Slump y Dragon Ball, y, en el mundo de los videojuegos, por el diseño de personajes de las franquicias Dragon Quest, Chrono Trigger y Blue Dragon. 
-        </p>
-        
-        <br>
-        <p>
-        Entró en 1973 en la Escuela Superior Industrial, inmediatamente después fue contratado por una empresa de publicidad en Nagoya, trabajó allí durante tres años, pero la falta de interés y la mala relación con sus jefes le condujo a la decisión de retirarse y a dedicarse al manga. Su vida de mangaka empezó con una publicación en Shonen Jump de Shūeisha. Más tarde crearía muchas otras historias para esa revista y, posteriormente recogidas en tomos. Toriyama se hizo inmensamente popular en Japón y en el extranjero gracias a su obra Dragon Ball, la cual ha sido vista por más de tres generaciones a escala mundial, y es Gokū y sus amigos, no sabía que tomarían gran fama afirma Akira Toriyama.
-</p>
-       <br> 
-        <p>
-        En Japón, sin embargo, continúa siendo una figura pública y aún vive en Aichi con su esposa e hijos, donde su fama le ha causado algunos problemas. Por ejemplo, cada vez que hay rumores de que hará alguna aparición en Tokio, la oficina de gobernación se preocupa porque, en ocasiones, multitudes de fans se aglomeran cerca de su casa para observarlo salir e incluso lo acompañan en caravana hasta el aeropuerto; este fenómeno dice mucho sobre la popularidad de Toriyama.
-        </p>
-        <br>
-        <p>
-        A pesar de su fama, en general, a Toriyama no le gusta exponerse en los medios de comunicación colectivos y son muy raras las ocasiones en las que algún periodista ha logrado entrevistarle o tomarle una fotografía para un diario extranjero. 
-        
-        </p>
-        
-        <br>
-        <br>
-        
-        <p class="stroke1">Bandai</p>
-        <br>
-        
-        <img src="img/bandai.png" height="250px" weight="250px"/>
-        
-        <p>
-        Bandai es una empresa japonesa dedicada a la creación de juguetes y figuras de acción, en muchos de los casos posee los derechos exclusivos para la promoción de mercadería basada de series animadas; es uno de los principales auspiciantes que han ayudado a poner en marcha diferentes series de anime
-
-        </p>
-        <br>
-        <br>
-        
-        <p class="stroke1">T</p><p class="stroke2">o</p><p class="stroke1">ei</p><p class="stroke3">Animati</p><p class="stroke2">o</p><p class="stroke3">n</p>
-        <br>
-        
-        <img src="img/Toei_Animation.svg.png" height="250px" weight="250px"/>
-        <p>
-        Toei Animation:
-        Toei Animation Company, Limited es un estudio de animación japonés propiedad de la empresa Toei Company, Limited. Ha creado varias series de televisión y películas y ha adaptado cómics japoneses como series animadas, muchas populares en todo el mundo. Toei es accionista de la cadena japonesa de televisión por satélite anime Animax junto con otros estudios de animación y productoras, como Sunrise, TMS Entertainment y Nihon Ad Systems Inc. La empresa tiene su sede en Ohizumi Studio en Nerima, Tokio. 
-        
-        
-        </p>
-        
-        
-        <br>
-        <p>
-        
-        
-        </p>
-            <footer>
-		      <p><strong>DragonBallWorld, página creada por Sergi Chismol Marí y Jorge Sendra Gómez.</strong></p>
-            </footer>
-                 
-        
-        
-    </body>
-
-</html>
-		
-		
